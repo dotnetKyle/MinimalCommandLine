eb8f15f8bf768ac7785cbe5ab0c2291d2ceed2bc
Kyle Burrows <dotnetKyle@gmail.com>
2022-08-21 20:48:16 -0400

    added a second version of RepeatHandler as an example

--- src/DemoApp/Handlers/RepeatHandler.cs
+++ src/DemoApp/Handlers/RepeatHandler.cs
@@ -1,12 +1,26 @@
-﻿namespace DemoApp.Handlers;
+﻿using System.CommandLine.Minimal;
+
+namespace DemoApp.Handlers;
 
 public static class RepeatHandler
 {
-    public static void Handle(string message, int repeatNumber)
+    public static async Task HandleAsync(string message, int repeatNumber, int waitTimesBetweenMessages = 150)
     {
         for (int i = 0; i < repeatNumber; i++)
         {
             Console.WriteLine(message);
+            await Task.Delay(waitTimesBetweenMessages);
         }
+    }
+}
+public static class RepeatHandler2
+{
+    public static Task HandleAsync(
+        [Argument] string message,
+        [Option] int repeatNumber,
+        [Option] int waitTimesBetweenMessagesInMs = 200
+        )
+    {
+        throw new NotImplementedException();
     }
 }
