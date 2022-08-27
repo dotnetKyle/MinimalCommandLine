60af541cf63a52163ead1b04519039ff7632fd52
Kyle Burrows <dotnetKyle@gmail.com>
2022-08-27 14:43:43 -0400

    Added a hello world app that demonstrates root command usage

---
+++ src/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.CommandLine.Minimal;
+
+var app = new MinimalCommandLineBuilder()
+    .Build();
+
+app.AddRootDescription("A simple demo app for the command line.")
+    .AddRootArgument<string>("Message")
+    .AddRootOption<string>("--first-option", opt => opt.AddAlias("-o1"))
+    .AddRootOption<string>("--second-option")
+    .SetRootHandler(
+        (string message, string option1, string option2) =>
+        {
+            Console.WriteLine($"Hello World!  {message}");
+            Console.WriteLine($"  Option 1:{option1}, Option2 {option2}");
+        }
+    );
+
+app.Execute(args);
