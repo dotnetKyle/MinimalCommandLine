112596fdb0b78bd8a5f82c5364d247db699e356f
Kyle Burrows <dotnetKyle@gmail.com>
2025-10-13 19:47:44 -0400

    Make collections Options by convention (#40)
    * Arrays now bind to Options by default
    * Other collection types now convert to options as well
    Fixes #31

--- src/System.CommandLine.Minimal/SourceGeneration/GeneratorBindingsProvider.cs
+++ src/System.CommandLine.Minimal/SourceGeneration/GeneratorBindingsProvider.cs
@@ -8,6 +8,8 @@ using System.Threading;
 
 namespace System.CommandLine.Minimal.SourceGeneration;
 
+internal enum BindingType { Argument, Option, DependencyInjection }
+
 internal static class GeneratorBindingsProvider
 {
     /// <summary>
@@ -35,7 +37,7 @@ internal static class GeneratorBindingsProvider
         foreach(IParameterSymbol param in parameters)
         {
             ImmutableArray<AttributeData> attributes = param.GetAttributes();
-            bool hasDefault = param.HasExplicitDefaultValue;
+
             string name = param.Name;
             string type = param.Type.ToDisplayString(NullableFlowState.None, SymbolDisplayFormat.FullyQualifiedFormat);
 
@@ -47,23 +49,20 @@ internal static class GeneratorBindingsProvider
                 ?.ToDisplayString() == "System.CommandLine.Minimal.ArgumentAttribute");
 
             // check for attributes first, then try to bind anyways
-            // by convention if it has a default value it is an option, devleoper can override this with an [Argument] attribute
+            // by convention if it has a default value it is an option, developer can override this with an [Argument] attribute
+            BindingType bindingType = DetermineBindingType(fromServicesAttribute, optionAttribute, argumentAttribute, param);
 
-            if (fromServicesAttribute is not null)
+            if (bindingType == BindingType.DependencyInjection)
             {
                 bindings.BindServiceDescriptor(name, type);
             }
-            else if(optionAttribute is not null)
+            else if(bindingType == BindingType.Option)
             {
-                bindings.BindOption(name, type, GetSymbolDefaultValue(ctx, param));
+                bindings.BindOption(name, type, GetSymbolDefaultValue(ctx, param), IsParamCollectionType(param));
             }
-            else if(argumentAttribute is not null || !hasDefault)
+            else if (bindingType == BindingType.Argument)
             {
-                bindings.BindArgument(name, type, GetSymbolDefaultValue(ctx, param));
-            }
-            else
-            {
-                bindings.BindOption(name, type, GetSymbolDefaultValue(ctx, param));
+                bindings.BindArgument(name, type, GetSymbolDefaultValue(ctx, param), IsParamCollectionType(param));
             }
         }
 
@@ -77,16 +76,77 @@ internal static class GeneratorBindingsProvider
             Bindings: bindings.ToImmutableArray());
     }
 
-    private static void BindOption(this List<ParameterBinding> list, string name, string type, string? defaultValueCode)
+    private static BindingType DetermineBindingType(
+        AttributeData? fromServicesAttribute,
+        AttributeData? optionAttribute,
+        AttributeData? argumentAttribute,
+        IParameterSymbol param)
+    {
+        if (fromServicesAttribute is not null)
+            return BindingType.DependencyInjection;
+        
+        if (optionAttribute is not null)
+            return BindingType.Option;
+        
+        if (argumentAttribute is not null)
+            return BindingType.Argument;
+        
+        // by convention, bind arrays to Options
+        if (param.Type.Kind == SymbolKind.ArrayType)
+            return BindingType.Option;
+        
+        // by convention, bind parameters with default values to Options
+        if (param.HasExplicitDefaultValue)
+            return BindingType.Option;
+        
+        // by convention, bind regular required parameters to Arguments
+        return BindingType.Argument;
+    }
+
+    private static bool IsParamCollectionType(IParameterSymbol param)
+    {
+        // specifically exclude strings from this list
+        if (param.Type.SpecialType == SpecialType.System_String)
+            return false;
+
+        if (param.Type.Kind == SymbolKind.ArrayType)
+            return true;
+
+        if (param.Type is INamedTypeSymbol namedType)
+        {
+            string typeName = namedType.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
+
+            foreach(INamedTypeSymbol @interface in namedType.AllInterfaces)
+            {
+                // Check if it's a constructed generic type like List<T>, IEnumerable<T>, etc.
+                var constructedFrom = namedType.ConstructedFrom.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
+
+                if (constructedFrom is
+                    "global::System.Collections.Generic.IEnumerable<T>" or
+                    "global::System.Collections.Generic.IList<T>" or
+                    "global::System.Collections.Generic.ICollection<T>" or
+                    "global::System.Collections.Generic.IReadOnlyList<T>" or
+                    "global::System.Collections.Generic.IReadOnlyCollection<T>" or
+                    "global::System.Collections.Generic.List<T>")
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static void BindOption(this List<ParameterBinding> list, string name, string type, string? defaultValueCode, bool isCollectionType)
     {
         // TODO: add aliases
-        OptionBinding option = new(name, type, defaultValueCode);
+        OptionBinding option = new(name, type, defaultValueCode, isCollectionType);
         list.Add(option);
     }
-    private static void BindArgument(this List<ParameterBinding> list, string name, string type, string? defaultValueCode)
+    private static void BindArgument(this List<ParameterBinding> list, string name, string type, string? defaultValueCode, bool isCollectionType)
     {
         // TODO: add description attribute if applicable
-        ArgumentBinding argument = new(name, type, defaultValueCode);
+        ArgumentBinding argument = new(name, type, defaultValueCode, isCollectionType);
         list.Add(argument);
     }
     private static void BindServiceDescriptor(this List<ParameterBinding> list, string name, string type)
@@ -171,7 +231,7 @@ internal record GeneratingCommandBinder(
     public string FullMethodName => $"{this.FullClassName}.{this.MethodName}";
 }
 
-internal abstract record ParameterBinding(string OriginalParameterName, string Type, string? DefaultValueConstant)
+internal abstract record ParameterBinding(string OriginalParameterName, string Type, string? DefaultValueConstant, bool IsCollectionType)
 {
     public string NameTitleCase
     {
@@ -190,25 +250,24 @@ internal abstract record ParameterBinding(string OriginalParameterName, string T
         }
     }
 }
-internal record ArgumentBinding(string OriginalParameterName, string Type, string? DefaultValueConstant)
-    : ParameterBinding(OriginalParameterName, Type, DefaultValueConstant)
+internal record ArgumentBinding(string OriginalParameterName, string Type, string? DefaultValueConstant, bool IsCollectionType)
+    : ParameterBinding(OriginalParameterName, Type, DefaultValueConstant, IsCollectionType)
 {
     /// <summary>
     /// The Argument name in title case, e.g. "myParameterName" becomes "My Parameter Name"
     /// </summary>
     public string ConventionalArgumentName 
-        => Conventions.ParameterNameConversion.ToArgumentName(this.OriginalParameterName);
+        => ParameterNameConversion.ToArgumentName(this.OriginalParameterName);
 }
-internal record OptionBinding(string OriginalParameterName, string Type, string? DefaultValueConstant)
-    : ParameterBinding(OriginalParameterName, Type, DefaultValueConstant)
+internal record OptionBinding(string OriginalParameterName, string Type, string? DefaultValueConstant, bool IsCollectionType)
+    : ParameterBinding(OriginalParameterName, Type, DefaultValueConstant, IsCollectionType)
 {
     /// <summary>
     /// The option name in kebab case, e.g. "myParameterName" becomes "--my-parameter-name"
     /// </summary>
-    public string OptionName 
-        => Conventions.ParameterNameConversion.ToOptionName(this.OriginalParameterName);
+    public string OptionName => ParameterNameConversion.ToOptionName(this.OriginalParameterName);
 }
 internal record FromServicesBinding(string OriginalParameterName, string Type)
-    : ParameterBinding(OriginalParameterName, Type, DefaultValueConstant:null)
+    : ParameterBinding(OriginalParameterName, Type, DefaultValueConstant:null, IsCollectionType:false)
 { 
 }
